c9639cb03bfeae3b910b946b02822dc65c4adf46
Amshu Belbase <amsubelbs@gmail.com>
2024-03-06 09:29:49 +0530

    adding all for v8

---
+++ predict.py
@@ -0,0 +1,66 @@
+import os
+
+from ultralytics import YOLO
+import cv2
+
+
+# VIDEOS_DIR = os.path.join('.', 'custom_data', 'images',
+#                           'val')  # address of video
+
+# VIDEOS_DIR = os.path.join('content', 'gdrive', 'MyDrive',
+#                           'DETECTION', 'yolov5', 'videos')  # address of video
+
+# /content/gdrive/MyDrive/DETECTION/yolov5/videos/video10.mp4
+# VIDEOS_DIR = os.path.join('.')
+# video_path = os.path.join(VIDEOS_DIR, 'video5.mp4')
+# video_path_out = '{}_out.mp4'.format(video_path)
+
+video_path = 0  # for webcam
+video_path_out = 'webcam_out.mp4'
+
+
+cap = cv2.VideoCapture(video_path)
+
+ret, frame = cap.read()
+H, W, _ = frame.shape
+
+if video_path != 0:
+    out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(
+        *'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+
+model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'best.pt')
+
+# Load a model
+model = YOLO(model_path)  # load a custom model
+
+threshold = 0.75
+
+while ret:
+
+    results = model(frame)[0]
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            cv2.rectangle(frame, (int(x1), int(y1)),
+                          (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3)
+
+    if video_path != 0:
+        out.write(frame)
+    # else:
+    #     cv2.imshow("output", frame)
+
+    ret, frame = cap.read()
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q') or key > -1:  # Handle both 'q' and any key press
+        print("Exiting the program...")
+        break
+
+
+cap.release()
+if video_path != 0:
+    out.release()
+cv2.destroyAllWindows()
